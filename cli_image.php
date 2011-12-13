6f5fd3b57d1d42422bd2033d91d5c193adad0d2a
Dean Farrell <farrelldlfarrell@gmail.com>
2011-12-13 15:13:52 -0500

    Adding command line script.

---
+++ cli_image.php
@@ -0,0 +1,23 @@
+<?php
+include 'constraint_functions.php';
+
+$db = dbConnect();
+	
+$query = "SELECT id, url FROM links WHERE screenshot_path IS NULL";
+$urls = $db->query($query);
+	
+if(empty($urls)) { 
+	echo 'There are no links to process'; 
+	exit;
+}
+	
+foreach($urls as $url) {
+	$link = trim($url['url']);
+	$file_path = "screenshots/" . $link . ".jpg";
+		
+	passthru('C:\"Program Files"\wkhtmltopdf\wkhtmltoimage.exe --zoom .75 --load-error-handling ignore --stop-slow-scripts --height 600 --width 450 http://' . $link . ' ' . $file_path);
+			
+	$query = "UPDATE links SET screenshot_path = ? WHERE id = ?";
+	$prepare = $db->prepare($query);
+	$run = $prepare->execute(array($file_path, $url['id']));
+}
